38940251c6fa7b657a6c32748aecbef3eface025
Bruno Joseph <bruno.joseph95@gmail.com>
2024-08-23 14:11:10 +0200

    Update download_weights.py

--- script/download_weights.py
+++ script/download_weights.py
@@ -1,4 +1,5 @@
-from diffusers import ControlNetModel, DiffusionPipeline, AutoencoderKL
+from diffusers import DiffusionPipeline, AutoencoderKL
+from diffusers.models.controlnet_flux import FluxControlNetModel
 import torch
 from RealESRGAN import RealESRGAN
 
@@ -10,15 +11,13 @@ for scale in [2, 4]:
 SD15_WEIGHTS = "weights"
 CONTROLNET_CACHE = "controlnet-cache"
 
-controlnet = ControlNetModel.from_pretrained(
-    "lllyasviel/control_v11f1e_sd15_tile", torch_dtype=torch.float16, cache_dir=CONTROLNET_CACHE
+controlnet = FluxControlNetModel.from_pretrained(
+    'InstantX/FLUX.1-dev-Controlnet-Union-alpha', torch_dtype=torch.bfloat16, cache_dir=CONTROLNET_CACHE
 )
 controlnet.save_pretrained(CONTROLNET_CACHE)
 
-vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-ema")
-
-pipe = DiffusionPipeline.from_pretrained(
-    "SG161222/Realistic_Vision_V5.1_noVAE", torch_dtype=torch.float16, cache_dir=SD15_WEIGHTS, vae=vae
+pipe = FluxPipeline.from_pretrained(
+    'black-forest-labs/FLUX.1-dev', torch_dtype=torch.bfloat16, cache_dir=SD15_WEIGHTS
 )
 pipe.save_pretrained(SD15_WEIGHTS)
 
